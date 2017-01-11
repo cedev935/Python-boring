e6501614cf09d015ff8cb5be0918c90d493d8886
Unknown <1229524609@qq.com>
2017-01-11 08:47:01 +0800

    修复一些问题

--- py/holiday.py
+++ py/holiday.py
@@ -1,3 +1,20 @@
-# tag:
-# rewrite-needed
-# many bugs
+import datetime,time,os,platform
+
+oss = platform.system()
+
+while True :
+	now_time = datetime.datetime.now()
+	final_test = datetime.datetime.strptime('2017-01-09  00:00:00', '%Y-%m-%d %H:%M:%S')
+	winter_hoilday = datetime.datetime.strptime('2017-01-13  00:00:00', '%Y-%m-%d %H:%M:%S')
+
+	print('距离17年期末考试还剩下')
+	print (final_test-now_time)
+	print('距离17年寒假还剩下')
+	print (winter_hoilday-now_time)
+
+	time.sleep(float(0.05))
+	if oss == "Windows":
+		os.system('cls')
+	else:
+		os.system('clear')
+	
