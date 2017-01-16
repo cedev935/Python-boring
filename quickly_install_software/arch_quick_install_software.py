19b76c21edc9da9fb0e47be2956cd03465b31ec9
LIZONGZE <919208140@qq.com>
2017-01-16 22:03:19 +0800

    Update arch_quick_install_software.py

--- quickly_install_software/arch_quick_install_software.py
+++ quickly_install_software/arch_quick_install_software.py
@@ -1,9 +1,9 @@
-# Version 0.1.0-alpha
+VERSION="0.1.0-alpha"
 import os,urllib.request,re,sys,requests
-print ('欢迎使用Arch软件快速安装工具,本程序能快速帮您安装国内的软件.')
-print ('输入1安装qq')
+print ('欢迎使用Arch软件快速安装工具,本程序能快速帮您安装国内的软件. 当前运行的版本为' + VERSION)
+print ('输入1安装QQ')
 print ('输入2安装百度云盘')
-print ('输入3安装vim配置')
+print ('输入3安装vim快速配置')
 print ('输入4安装网易云音乐')
 print ('输入5安装wine rar（当系统自带解压缩软件遇到乱码时使用）')
 print ('输入6安装经典wine扫雷')
