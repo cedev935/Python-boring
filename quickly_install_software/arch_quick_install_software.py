b648fc4bf4237847a2c70cb00a195f9be7499c1b
redapple0204 <1229524609@qq.com>
2017-01-16 21:41:39 +0800

    Update arch_quick_install_software.py

--- quickly_install_software/arch_quick_install_software.py
+++ quickly_install_software/arch_quick_install_software.py
@@ -1,3 +1,6 @@
 import os,urllib.request,re,sys,requests
-import ('欢迎使用arch软件快速安装工具')
-import ('本程序能快速帮您安装国内的软件，例如wineqq，百度网盘，vim配置等')
+print ('欢迎使用arch软件快速安装工具')
+print ('本程序能快速帮您安装国内的软件，例如wineqq，百度网盘，vim配置等')
+print ('输入1安装qq')
+print ('输入2安装百度云盘')
+print 
