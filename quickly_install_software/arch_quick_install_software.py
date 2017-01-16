8ea55741f968e4dbc1a5340ec8191d833d6073eb
LIZONGZE <919208140@qq.com>
2017-01-16 22:01:20 +0800

    Update arch_quick_install_software.py

--- quickly_install_software/arch_quick_install_software.py
+++ quickly_install_software/arch_quick_install_software.py
@@ -1,6 +1,6 @@
+# Version 0.1.0-alpha
 import os,urllib.request,re,sys,requests
-print ('欢迎使用arch软件快速安装工具')
-print ('本程序能快速帮您安装国内的软件，例如wineqq，百度网盘，vim配置等')
+print ('欢迎使用Arch软件快速安装工具,本程序能快速帮您安装国内的软件.')
 print ('输入1安装qq')
 print ('输入2安装百度云盘')
 print ('输入3安装vim配置')
