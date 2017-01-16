a7f7671bf5417c955c946ddc84135fc97b732f30
redapple0204 <1229524609@qq.com>
2017-01-16 21:43:12 +0800

    Update arch_quick_install_software.py

--- quickly_install_software/arch_quick_install_software.py
+++ quickly_install_software/arch_quick_install_software.py
@@ -3,4 +3,6 @@ print ('欢迎使用arch软件快速安装工具')
 print ('本程序能快速帮您安装国内的软件，例如wineqq，百度网盘，vim配置等')
 print ('输入1安装qq')
 print ('输入2安装百度云盘')
-print 
+print ('输入3安装vim配置')
+print ('输入4安装网易云音乐')
+print ('输入5安装wine rar（当系统自带解压缩软件遇到乱码时使用）
