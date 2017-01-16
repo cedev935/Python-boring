d08314738dcde25ba3f7720531aa4c25efc6bad9
redapple0204 <1229524609@qq.com>
2017-01-16 21:48:56 +0800

    Update arch_quick_install_software.py

--- quickly_install_software/arch_quick_install_software.py
+++ quickly_install_software/arch_quick_install_software.py
@@ -5,4 +5,6 @@ print ('输入1安装qq')
 print ('输入2安装百度云盘')
 print ('输入3安装vim配置')
 print ('输入4安装网易云音乐')
-print ('输入5安装wine rar（当系统自带解压缩软件遇到乱码时使用）
+print ('输入5安装wine rar（当系统自带解压缩软件遇到乱码时使用）')
+print ('输入6安装经典wine扫雷')
+print ('输入
