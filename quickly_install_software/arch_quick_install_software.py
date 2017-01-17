c50a2d7e1bf8ecdc453c48f2cfc740b36165a0b4
Unknown <1229524609@qq.com>
2017-01-17 10:23:39 +0800

    update

--- quickly_install_software/arch_quick_install_software.py
+++ quickly_install_software/arch_quick_install_software.py
@@ -1,14 +1,6 @@
 VERSION="0.1.0-alpha"
 RELEASE_DATE="20170116"
 import os,urllib.request,re,sys,requests,urllib.request
-
-def download( link, savename ):
-	netf = requests.get(link)
-	fdata = urllib.request.urlopen(netf).read()
-	with open(savename, 'wb') as f:
-		f.write(fdata)
-
-
 print ('欢迎使用Arch软件快速安装工具,本程序能快速帮您安装国内的软件. 当前运行的版本为' + VERSION + " / " + RELEASE_DATE)
 print ('输入1安装QQ')
 print ('输入2安装百度云盘')
@@ -30,5 +22,7 @@ if choice == "1":
 choice_qq = input()
 if choice_qq == "1":
 	print ('正在下载安装中，请稍等....')
+	print ('本程序暂不支持显示进度，本文件大小大约为60M，如需查看进度，可打开系统监视器查看下载速度')
 	urllib.request.urlretrieve("http://mirrors.ustc.edu.cn/deepin/pool/non-free/c/crossover-15/crossover-15_15.0.5-1_i386.deb", "deepin_crossover.deb")
+	print ('下载完毕，正在准备安装')
 	
