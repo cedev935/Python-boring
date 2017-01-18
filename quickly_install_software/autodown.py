0608a87c3f08e3432d37110e07a52c89de831834
LIZONGZE <919208140@qq.com>
2017-01-18 16:54:02 +0800

    Update autodown.py

--- quickly_install_software/autodown.py
+++ quickly_install_software/autodown.py
@@ -1,9 +1,9 @@
-﻿# Python 3.6+
+# Python 3.6+
 
 VERSION="0.1.0-alpha"
 RELEASE_DATE="20170118"
 
-import json,os
+import json,os,urllib.request,platform
 
 def readFile(filepth):
     with open(filepth) as f:
@@ -20,8 +20,23 @@ def walkFiles(pth):
         for filename in filenames:
             result.append(filename)
     return result
+    
+def downloadFiles(link, saveName):
+    urllib.request.urlretrieve(link, f"./{saveName}")
+
+def downloadAll(jsonD, platf):
+    downlink = jsonD["install"]["download"][platf]
+    for files in downlink:
+        downloadFiles(files["link"], files["saveName"])
+        
+def runSteps(jsonD, platf):
+    steps = jsonD["install"]["setup"][platf]
+    for step in steps: exec(step)
 
 def Emain():
+    if platform.system() != ("Windows" or "Linux"):
+        print("Sorry, this platform can't run the programme.")
+        exit()
     print("Welcome to use autodown.py")
     print(f"Version: {VERSION} / {RELEASE_DATE}")
     print("Start an issue on https://github.com/redapple0204/my-boring-python/issues")
@@ -41,6 +56,11 @@ def Emain():
     next = input("Do you want to continue?[Y/n]")
     if next != "Y":
         exit()
-    
+    print("Downloading files...")
+    downloadAll(install_data, platform.system())
+    print("Download finished")
+    print("Setting up...")
+    runSteps(install_data, platform.system())
+    print("Finished")
+
 Emain()
-    
