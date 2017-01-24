7f0936c8712e241e03d26ba2a2628bea43bdab93
Unknown <1229524609@qq.com>
2017-01-24 12:14:29 +0800

    update hoilday.py

--- py/holiday.py
+++ py/holiday.py
@@ -4,13 +4,23 @@ oss = platform.system()
 
 while True :
 	now_time = datetime.datetime.now()
-	final_test = datetime.datetime.strptime('2017-01-09  00:00:00', '%Y-%m-%d %H:%M:%S')
-	winter_hoilday = datetime.datetime.strptime('2017-01-13  00:00:00', '%Y-%m-%d %H:%M:%S')
+	winter_hoilday_boom = datetime.datetime.strptime('2017-02-12  14:00:00', '%Y-%m-%d %H:%M:%S')
+	winter_hoilday_boom_summer_number = 31-(winter_hoilday_boom-now_time).days
+	QM = datetime.datetime.strptime('2017-04-02  00:00:00', '%Y-%m-%d %H:%M:%S')
+	LD = datetime.datetime.strptime('2017-04-29  00:00:00', '%Y-%m-%d %H:%M:%S')
+	DW = datetime.datetime.strptime('2017-05-28  00:00:00', '%Y-%m-%d %H:%M:%S')
+	print('距离寒假结束还剩下')
+	print (winter_hoilday_boom-now_time)
+	print('今天相当于暑假的')
+	print('8月',winter_hoilday_boom_summer_number,'日')
+	print('距离清明节还剩下')
+	print (QM-now_time)
+	print('距离劳动节还剩下')
+	print (LD-now_time)
+	print('距离端午节还剩下')
+	print (DW-now_time)
+	
 
-	print('距离17年期末考试还剩下')
-	print (final_test-now_time)
-	print('距离17年寒假还剩下')
-	print (winter_hoilday-now_time)
 
 	time.sleep(float(0.05))
 	if oss == "Windows":
