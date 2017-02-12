aba8d2773bf94b285326456cf99f65e425d3bcf3
LIZONGZE <919208140@qq.com>
2017-02-12 18:13:23 +0800

    import datetime as dt but why not use

--- py/holiday.py
+++ py/holiday.py
@@ -5,14 +5,14 @@ operating_system = platform.system() #K-Guan建议
 
 while True :
 	now_time = datetime.datetime.now()
-#	winter_hoilday_boom = datetime.datetime.strptime('2017-02-12  13:00:00', '%Y-%m-%d %H:%M:%S') #组长需要提前1小时到校
-#	winter_hoilday_boom_c = datetime.datetime.strptime('2017-02-13  00:00:00', '%Y-%m-%d %H:%M:%S')
+#	winter_hoilday_boom = dt.datetime.strptime('2017-02-12  13:00:00', '%Y-%m-%d %H:%M:%S') #组长需要提前1小时到校
+#	winter_hoilday_boom_c = dt.datetime.strptime('2017-02-13  00:00:00', '%Y-%m-%d %H:%M:%S')
 #	winter_hoilday_boom_summer_number = 31-(winter_hoilday_boom_c-now_time).days
-#	winter_hoilday_start = datetime.datetime.strptime('2017-01-13  00:00:00', '%Y-%m-%d %H:%M:%S')
+#	winter_hoilday_start = dt.datetime.strptime('2017-01-13  00:00:00', '%Y-%m-%d %H:%M:%S')
 #	winter_hoilday_boom_BFB = dt.datetime(2017,1,13,00,00,00)
-	QM = datetime.datetime.strptime('2017-04-02  00:00:00', '%Y-%m-%d %H:%M:%S')
-	LD = datetime.datetime.strptime('2017-04-29  00:00:00', '%Y-%m-%d %H:%M:%S')
-	DW = datetime.datetime.strptime('2017-05-28  00:00:00', '%Y-%m-%d %H:%M:%S')
+	QM = dt.datetime.strptime('2017-04-02  00:00:00', '%Y-%m-%d %H:%M:%S')
+	LD = dt.datetime.strptime('2017-04-29  00:00:00', '%Y-%m-%d %H:%M:%S')
+	DW = dt.datetime.strptime('2017-05-28  00:00:00', '%Y-%m-%d %H:%M:%S')
 #	print('距离寒假结束还剩下')
 #	print (winter_hoilday_boom-now_time)
 #	print('今天相当于暑假的')
