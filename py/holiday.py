d9ee954cfccc0866146049e8d7103b4074006531
Unknown <1229524609@qq.com>
2017-02-12 11:18:55 +0800

    tmd寒假就结束了？？？好吧只好注释寒假段落

--- py/holiday.py
+++ py/holiday.py
@@ -5,20 +5,20 @@ operating_system = platform.system() #K-Guan建议
 
 while True :
 	now_time = datetime.datetime.now()
-	winter_hoilday_boom = datetime.datetime.strptime('2017-02-12  13:00:00', '%Y-%m-%d %H:%M:%S') #组长需要提前1小时到校
-	winter_hoilday_boom_c = datetime.datetime.strptime('2017-02-13  00:00:00', '%Y-%m-%d %H:%M:%S')
-	winter_hoilday_boom_summer_number = 31-(winter_hoilday_boom_c-now_time).days
-	winter_hoilday_start = datetime.datetime.strptime('2017-01-13  00:00:00', '%Y-%m-%d %H:%M:%S')
-	winter_hoilday_boom_BFB = dt.datetime(2017,1,13,00,00,00)
+#	winter_hoilday_boom = datetime.datetime.strptime('2017-02-12  13:00:00', '%Y-%m-%d %H:%M:%S') #组长需要提前1小时到校
+#	winter_hoilday_boom_c = datetime.datetime.strptime('2017-02-13  00:00:00', '%Y-%m-%d %H:%M:%S')
+#	winter_hoilday_boom_summer_number = 31-(winter_hoilday_boom_c-now_time).days
+#	winter_hoilday_start = datetime.datetime.strptime('2017-01-13  00:00:00', '%Y-%m-%d %H:%M:%S')
+#	winter_hoilday_boom_BFB = dt.datetime(2017,1,13,00,00,00)
 	QM = datetime.datetime.strptime('2017-04-02  00:00:00', '%Y-%m-%d %H:%M:%S')
 	LD = datetime.datetime.strptime('2017-04-29  00:00:00', '%Y-%m-%d %H:%M:%S')
 	DW = datetime.datetime.strptime('2017-05-28  00:00:00', '%Y-%m-%d %H:%M:%S')
-	print('距离寒假结束还剩下')
-	print (winter_hoilday_boom-now_time)
-	print('今天相当于暑假的')
-	print('8月',winter_hoilday_boom_summer_number,'日')
-	print('寒假已经过去了')
-	print (((now_time-winter_hoilday_boom_BFB).total_seconds())*100/2631600,'%') #组长需要提前1小时到校
+#	print('距离寒假结束还剩下')
+#	print (winter_hoilday_boom-now_time)
+#	print('今天相当于暑假的')
+#	print('8月',winter_hoilday_boom_summer_number,'日')
+#	print('寒假已经过去了')
+#	print (((now_time-winter_hoilday_boom_BFB).total_seconds())*100/2631600,'%') #组长需要提前1小时到校
 	print('距离清明节还剩下')
 	print (QM-now_time)
 	print('距离劳动节还剩下')
