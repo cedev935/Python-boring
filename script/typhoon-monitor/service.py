cd9afb53be919b16a37ef92cbc29fd06f1a45a55
cedev935 <cedev935@gmail.com>
2017-08-15 21:35:43 +0800

    修复了由于贴吧最热引来的错误

--- script/typhoon-monitor/service.py
+++ script/typhoon-monitor/service.py
@@ -3,11 +3,14 @@ data = urllib.request.urlopen('https://tieba.baidu.com/f?kw=%E5%8F%B0%E9%A3%8E&i
 change = data.decode('utf-8') 
 pattern = re.compile('【讨论[台|低][风|压]】.*')
 typhoon = re.findall(pattern, change)
+del(typhoon[0])
+
 while True:	
 	data2 = urllib.request.urlopen('https://tieba.baidu.com/f?kw=%E5%8F%B0%E9%A3%8E&ie=utf-8&tab=good').read()
 	change2 = data2.decode('utf-8') 
 	pattern2 = re.compile('【讨论[台|低][风|压]】.*')
 	typhoon2 = re.findall(pattern, change2)
+	del(typhoon2[0])
 	
 	if typhoon == typhoon2:
 		print("no")
@@ -18,5 +21,6 @@ while True:
 		change = data.decode('utf-8') 
 		pattern = re.compile('【讨论[台|低][风|压]】.*')
 		typhoon = re.findall(pattern, change)
+		del(typhoon[0])
 		time.sleep(1000)
 		
