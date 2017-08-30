7e25506e5911fe290459fd9fc0d57a54b99e1843
LIZONGZE <919208140@qq.com>
2017-08-30 17:54:34 +0800

    v1.1

--- gdrive-proxy/main.go
+++ gdrive-proxy/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
 )
 
 type DriveFile struct {
@@ -11,36 +23,98 @@ type DriveFile struct {
 	APIUrl string
 }
 
-type AccountInfo struct {
-	Access_Key string
-}
-
 func (driveFile *DriveFile) getAPIURL() {
 	driveFile.APIUrl = "https://www.googleapis.com/drive/v3/files/" + driveFile.ID + "?alt=media"
 }
 
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+"authorization code: \n%v\n", authURL)
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Println("Unable to retrieve token from web %v", err)
+
+	}
+	return tok
+}
+
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("drive-go-quickstart.json")), err
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
 func streamHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: &v", err)
+	}
+	client := getClient(ctx, config)
 	r.ParseForm()
 	fileID := r.Form.Get("id")
-	access_key := r.Form.Get("access_key")
 	driveFile := &DriveFile{
 		ID: fileID,
 	}
 	driveFile.getAPIURL()
-	accountInfo := &AccountInfo{access_key}
-	httpClient := &http.Client{}
 	req, err := http.NewRequest("GET", driveFile.APIUrl, nil)
 	if err != nil {
 		log.Println("[ERROR]", err.Error())
 		return
 	}
-	req.Header.Add("Authorization", "Bearer "+accountInfo.Access_Key)
-	resp, err := httpClient.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("[ERROR]", err.Error())
 		return
 	}
-	defer resp.Body.Close()
 	io.Copy(w, resp.Body)
 }
 
