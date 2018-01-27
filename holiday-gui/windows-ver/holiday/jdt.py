86f76baf8fe4da3443d0902cc5071af164ca245a
cedev935 <cedev935@gmail.com>
2018-01-27 13:56:24 +0800

    统配win和linux

---
+++ holiday-gui/windows-ver/holiday/jdt.py
@@ -0,0 +1,18 @@
+
+import sys,os
+import time
+
+def view_bar(num, total):
+  rate = num / total
+  rate_num = int(rate * 100)
+  r = '\r[%s%s]%d%%' % ("="*num, " "*(100-num), rate_num, )
+  sys.stdout.write(r)
+  sys.stdout.flush()
+
+bfb = open(r"C:\Users\redapple\.holiday\bfb.txt", "r").read()
+
+if __name__ == '__main__':
+    i = int(bfb)
+    time.sleep(0.1)
+    view_bar(i, 100)
+
