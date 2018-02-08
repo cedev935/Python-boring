503fdc6babb0e7cce5b6c46c98bab40d1e148843
cedev935 <cedev935@gmail.com>
2018-02-08 17:06:14 +0800

    添加两个功能：显示星期和显示直观日历

--- holiday-gui/linux-ver/holiday-gui.py
+++ holiday-gui/linux-ver/holiday-gui.py
@@ -1,4 +1,4 @@
-import datetime,easygui,math,os,re,sys,time #导入模块
+import datetime,easygui,math,os,re,sys,time,calendar #导入模块
 while True: #循环
 	summer_holiday = datetime.datetime(2018, 2, 25) #暑假结束时间
 	summer_holiday_start = datetime.datetime(2018, 1, 26) #暑假开始时间
@@ -33,7 +33,49 @@ while True: #循环
 		print_summer = "7月" + str(convent_summer_seven) #就是相当于暑假的七月
 	else: #反之
 		print_summer = "8月" + str(convent_summer) #就是相当于暑假的八月
-	tips = "今天是" + str(today) + "\n" + "距离寒假结束还有" + str(summer_holiday_last_ture) + "天" + "\n" + "寒假已经过去了" + "\n" + str(jdt) + "\n" + "现在相当于暑假的" + str(print_summer) + "日" + "\n" + "寒假作业已经完成了" + "\n" + str(homework_jindutiao) #配置窗口要显示的内容
+	basic_day = datetime.date.today() #导入今天
+	check_day = datetime.date(2018,2,25) #导入结束的日期
+	days_gap  = (check_day-basic_day).days-(7-basic_day.isoweekday()) #计算差几个周
+	#math.ceil(days_gap/7---->判断是日期差几个周
+	#check_day.isoweekday() ---->获取周几
+	number = str(check_day.isoweekday()) #转换str
+	if number == "1": #一系列判断
+		weekday = "一"
+	elif number == "2":
+		weekday = "二"
+	elif number == "3":
+		weekday = "三"
+	elif number == "4":
+		weekday = "四"
+	elif number == "5":
+		weekday = "五"
+	elif number == "6":
+		weekday = "六"
+	elif number == "7":
+		weekday = "日"
+	today_weekday = datetime.datetime.today().strftime("%w"); #今天星期几(数字)
+	if today_weekday == "1": #判断
+		today_weekday = "一"
+	elif today_weekday == "2":
+		today_weekday = "二"
+	elif today_weekday == "3":
+		today_weekday = "三"
+	elif today_weekday == "4":
+		today_weekday = "四"
+	elif today_weekday == "5":
+		today_weekday = "五"
+	elif today_weekday == "6":
+		today_weekday = "六"
+	elif today_weekday == "0":
+		today_weekday = "日"
+	calc_weekday = "寒假将在"+('下'*math.ceil(days_gap/7)+'周'+weekday)+"结束"+"，"+"并且今天是星期"+str(today_weekday) #输出星期计算
+	cal = calendar.month(2018, 2) #导入图形日历
+	remove_egl = cal.replace("February 2018","二零一八年二月") #删除最顶上的日历以免替换的时候有歧义
+	today_false = datetime.date.today() #倒入今天日期
+	today_true = str(today_false.day) #str今天日期（精确到日）
+	change_nowday = remove_egl.replace(today_true+" ","今") #替换今天日期
+	change_all = change_nowday.replace("25","毕") #替换假期结束日期
+	tips = "今天是" + str(today) + "\n" + "距离寒假结束还有" + str(summer_holiday_last_ture) + "天" + "\n" + "寒假已经过去了" + "\n" + str(jdt) + "\n" + "现在相当于暑假的" + str(print_summer) + "日" + "\n" + "寒假作业已经完成了" + "\n" + str(homework_jindutiao) + "\n" + calc_weekday + "\n" + "日历显示如下" + "\n" + change_all  #配置窗口要显示的内容
 	tips_finally = easygui.codebox(tips,"倒计时小程序",file) #显示窗口
 	file.close() #关闭文件
 	if tips_finally == None: #如果用户按了关闭或取消
