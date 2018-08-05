115aae194f2a6c98e26a4f4b16574da8ab6fae84
cedev935 <cedev935@gmail.com>
2018-08-05 21:52:31 +0800

    更新进度条显示方法（不喜欢的可以回退）

--- holiday-gui/windows-ver/holiday/jdt.py
+++ holiday-gui/windows-ver/holiday/jdt.py
@@ -2,5 +2,8 @@ bfb_raw = open(r"C:\Users\redapple\holiday\bfb.txt", "r").read()
 bfb = int(bfb_raw)
 b = int((bfb)/2)
 a = int(50-b)
-c = ("["+"="*b+" "*a+"]"+str(bfb)+"%")
+if str(b) == "0":
+	c = ("["+"-"*b+" "*a+"]"+str(bfb)+"%")
+else:
+	c = ("["+"-"*(b-1)+">"+" "*a+"]"+str(bfb)+"%")
 print(c)
