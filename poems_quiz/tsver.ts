62f0199dd4d8c50e3b626a752843550acc14ce15
LIZONGZE <neteroster@gmail.com>
2019-11-10 10:57:15 +0800

    Update tsver.ts

--- poems_quiz/tsver.ts
+++ poems_quiz/tsver.ts
@@ -1,21 +1,31 @@
-const a = poems.split("++++++++++++++++")
-    .map((poem) => {
-        const sent = poem.split(/。|】/).filter((sent) => {
-            if (sent.includes("【")) { return false }
-            return true
-        })
-        return sent.filter((sent) => {
-            if (sent.split("，").length != 2) {
-                return false
-            } return true
-        }).map((sents) => {
-            const rand = Math.random()
-            if (rand < 0.5) { return sents.split("，")[0] + "，_______。" }
-            if (rand >= 0.5) { return "_______，" + sents.split("，")[1] + "。" }
-            return ""
-        } )
-    })
+const randomChoose = (sents: string) => {
+    const rand = Math.random()
+    if (rand < 0.5) { return sents.split("，")[0] + "，_______。" }
+    if (rand >= 0.5) { return "_______，" + sents.split("，")[1] + "。" }
+    return ""
+}
 
-a.forEach((eachs) => {
+const sentenceSplit = (poem: string) => {
+    const sent = poem.split(/。|】/)
+    return sent
+}
+
+const sentenceFilter = (sentence: string) => {
+    if (sentence.includes("【")) { return false }
+    if (sentence.split("，").length != 2 ) {return false}
+    return true
+}
+
+const poemFilter = (poem: string) => {
+    return true
+}
+
+const result = poems.split("++++++++++++++++")
+    .filter(poemFilter)
+    .map(sentenceSplit)
+    .map((splitedPoem) => splitedPoem.filter(sentenceFilter))
+    .map((splitedPoem) => splitedPoem.map(randomChoose))
+
+result.forEach((eachs) => {
     eachs.forEach((t) => console.log(t))
 })
