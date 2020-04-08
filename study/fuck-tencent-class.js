7b494b0aede0efdf6a0084f95269c88973b32c66
cedev935 <cedev935@gmail.com>
2020-04-08 11:56:57 +0800

    Update fuck-tencent-class.js

--- study/fuck-tencent-class.js
+++ study/fuck-tencent-class.js
@@ -19,17 +19,52 @@ window.addEventListener('visibilitychange', function handle(e) {
 }, true);
 
 // Force include jQuery.
+// ==UserScript==
+// @name        New script 
+// @namespace   Violentmonkey Scripts
+// @match       *://*.ke.qq.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 2020/2/9 上午10:28:50
+// ==/UserScript==
+// Force include jQuery.
 var jQscript = document.createElement('script');
 jQscript.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 jQscript.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(jQscript);
 
+//If you'd like to use auto choice answer function,please remove "//"
+
+//Load randojs to choice answer randomly
+//var random_script = document.createElement('script');
+//random_script.src = 'https://randojs.com/1.0.0.js';
+//random_script.type = 'text/javascript';
+//document.getElementsByTagName('head')[0].appendChild(random_script);
+
 function goSign() {
     try {
         $('.s-btn.s-btn--primary.s-btn--m').click();
-// WARNING: Automatic selection of answer card options
+//If you'd like to use auto choice answer function,please remove "//"
+
+//WARNING:IT WILL CHOICE ANSWER **RANDOMLY** WHEN TEACHER POST ON AN ANSWER CARD.
 //        setTimeout(function () {
-//            $('.s-f-rc-item').click();
+//            var datiElements = document.getElementsByClassName("head-text");
+//            for(var e of datiElements){if(e.innerHTML.indexOf("单选题") !== -1){
+//	            var number = document.getElementsByClassName("s-f-rc-item").length
+//	            var random = Math.floor(Math.random()*number);
+//	            document.getElementsByClassName("s-f-rc-item")[random].click();
+//	            }
+//            }
+//            for(var e of datiElements){if(e.innerHTML.indexOf("多选题") !== -1){
+//	            var number = document.getElementsByClassName("s-f-rc-item").length
+//	            var random = randoSequence(0,number);
+//	            var random_1 = random[0]
+//	            var random_2 = random[1]
+//	            document.getElementsByClassName("s-f-rc-item")[random_1].click();
+//	            document.getElementsByClassName("s-f-rc-item")[random_2].click();
+//	            }
+//            }
 //        },1000)
     } catch(e) {
         console.log(e)
